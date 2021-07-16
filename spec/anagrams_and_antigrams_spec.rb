a49fe46fae0beaef6e4bd7a8ed64b80ff3ed6ef8
Kris Hopper <hopperdavis@gmail.com>
2021-07-16 09:08:31 -0700

    failed second test

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -8,4 +8,9 @@ describe('#anagrams') do
     word = Anagram.new("ruby")
     expect(word.check_anagram).to(eq(["r", "u", "b", "y"]))
   end
+  
+  it("returns an array of letters in a given sentence, ignoring spaces") do
+    word = Anagram.new("hello world")
+    expect(word.check_anagram).to(eq(["h", "e", "l", "l", "o", "w", "o", "r", "l", "d"]))
+  end
 end
