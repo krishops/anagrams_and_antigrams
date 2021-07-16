143172a9c812905853608ba3ee3ceedee90460b3
Kris Hopper <hopperdavis@gmail.com>
2021-07-16 13:46:24 -0700

    pass tests 5 and 6

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -9,8 +9,10 @@ class Anagram
     word2_array = @word2.downcase.split("").sort
     if ((@word1.downcase.scan(/[aeiou]/i).length == 0) && (@word2.downcase.scan(/[aeiou]/i).length == 0))
       "You need to input actual words"
-    elsif (word1_array.all?(word2_array) == false)
-      # literals?
+    elsif ((word1_array.any? {|word1| word2_array.include?(word1)}) == false)
+      
+      # (word1_array.all?(word2_array) == false)
+  
       # binding.pry
       # (@word1.downcase.scan(//) != @word2.downcase.scan(//))
       "These words have no letter matches and are antigrams"
@@ -22,6 +24,9 @@ class Anagram
   end
 end
 
+
+# word1_array.each do |letter, i|
+#   if letter = word2_array[i]
 
 
   # word1_no_space = @word1.delete(' ')
