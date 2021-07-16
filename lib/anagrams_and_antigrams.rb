6b997a0cfc04193de9a3d5333179d415e1248d57
Kris Hopper <hopperdavis@gmail.com>
2021-07-16 14:50:47 -0700

    pass eighth test

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -5,16 +5,18 @@ class Anagram
   end
 
   def check_anagram
-    word1_array = @word1.delete(' ').downcase.split("").sort
-    word2_array = @word2.delete(' ').downcase.split("").sort
+    word1_array = @word1.downcase.delete('^a-z').split("").sort
+    word2_array = @word2.downcase.delete('^a-z').split("").sort
     if ((@word1.downcase.scan(/[aeiou]/i).length == 0) && (@word2.downcase.scan(/[aeiou]/i).length == 0))
+      
       "You need to input actual words"
     elsif ((word1_array.any? {|word1| word2_array.include?(word1)}) == false)
       "These words have no letter matches and are antigrams"
     elsif (word1_array == word2_array)
       "These words are anagrams"
-    else
+    elsif (word1_array != word2_array)
       "These words are not anagrams or antigrams"
+
     end
   end
 end
